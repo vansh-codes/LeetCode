aa3acad97db80ca14126e603e598c37c2911c3b4
Vansh Chaurasiya <114163734+vansh-codes@users.noreply.github.com>
2024-12-03 12:41:44 +0530

    Time: 20 ms (53.01%), Space: 85.5 MB (18.48%) - LeetHub

--- 2109-adding-spaces-to-a-string/2109-adding-spaces-to-a-string.cpp
+++ 2109-adding-spaces-to-a-string/2109-adding-spaces-to-a-string.cpp
@@ -1,9 +1,21 @@
 class Solution {
 public:
     string addSpaces(string s, vector<int>& spaces) {
-        for(int i=0;i<spaces.size();i++){
-            s.insert(s.begin() + spaces[i] + i, ' ');
+        // TLE
+        // for(int i=0;i<spaces.size();i++){
+        //     s.insert(s.begin() + spaces[i] + i, ' ');
+        // }
+        // return s;
+        
+        string ans = "";
+        int pointer = 0;
+        for(int i=0;i<s.size();i++){
+            if(pointer<spaces.size() && i==spaces[pointer]){
+                ans += " ";
+                pointer++;
+            }
+            ans += s[i]; 
         }
-        return s;
+        return ans;
     }
 };
