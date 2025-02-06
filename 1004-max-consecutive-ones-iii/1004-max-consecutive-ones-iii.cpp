c5d0a3af6eff6f6005cba9e083b68ef4bc6e89d0
Vansh Chaurasiya <114163734+vansh-codes@users.noreply.github.com>
2025-02-06 11:33:53 +0530

    Time: 0 ms (100%), Space: 59.4 MB (5.26%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
@@ -1,20 +1,36 @@
 class Solution {
 public:
+    // view notes for approach
     int longestOnes(vector<int>& nums, int k) {
         int l = 0, r = 0, zeroes = 0, maxLen = 0, n = nums.size();
-        while(r<n){
-            if(nums[r]==0){
+        // O(2N) Solution \U0001f447
+        /* while (r < n) { // O(N)
+            if (nums[r] == 0) {
                 zeroes++;
-                if(zeroes>k){
-                    while(zeroes>k){
-                        if(nums[l]==0){
-                            zeroes--;
-                        }
-                        l++;
+                while (zeroes > k) {    // O(N)
+                    if (nums[l] == 0) {
+                        zeroes--;
                     }
+                    l++;
                 }
             }
             maxLen = max(maxLen, r - l + 1);
+            r++;
+        } */
+
+        // O(N) Solution \U0001f447
+        while (r < n) {
+            if (nums[r] == 0) {
+                zeroes++;
+            }
+            if (zeroes > k) {       // if zeroes > k then move the left pointer
+                if (nums[l] == 0)   // if zero encountered then decrement zero count and move left pointer
+                    zeroes--;
+                l++;
+            }
+            if (zeroes <= k) {      // if zeroes <= k, then only compute maxLen
+                maxLen = max(maxLen, r - l + 1);
+            }
             r++;
         }
         return maxLen;
