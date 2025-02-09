c99f18ad8bdccf374b1854cd6735f2acf28fd580
Vansh Chaurasiya <114163734+vansh-codes@users.noreply.github.com>
2025-02-10 00:21:56 +0530

    Time: 0 ms (100%), Space: 11.1 MB (11.24%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
@@ -19,9 +19,26 @@ private:
         return;
     }
 public:
+    // iterative
     vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> res;
+        if(!root) return res;
+        stack<TreeNode*> st;
+        st.push(root);
+        while(!st.empty()){
+            root = st.top();
+            st.pop();
+            res.push_back(root->val);
+            if(root->right) st.push(root->right);
+            if(root->left) st.push(root->left);
+        }
+        return res;
+    }
+
+    // recursive
+    /* vector<int> preorderTraversal(TreeNode* root) {
         vector<int> res;
         helper(root, res);
         return res;
-    }
+    } */
 };
