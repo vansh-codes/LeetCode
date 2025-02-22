e2836f2a82b3a31776e173e0b5feb703f8ef649d
Vansh Chaurasiya <114163734+vansh-codes@users.noreply.github.com>
2025-02-22 23:43:30 +0530

    Time: 0 ms (100%), Space: 35.6 MB (27.12%) - LeetHub

--- 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.cpp
+++ 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.cpp
@@ -6,32 +6,21 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
     TreeNode* searchBST(TreeNode* root, int val) {
-        if(root==NULL)
-            {
-                return NULL;
-            }
-        else
-            {
-                if(root->val == val)
-                    {
-                        return root;
-                    }
-                else if(root->val < val)
-                    {
-                        return searchBST(root->right,val);
-                    }
-                else
-                    {
-                        return searchBST(root->left,val);
-                    }
-                return NULL;
-            }
-        return NULL;
+        if (root == nullptr || root->val == val) {
+            return root;
+        }
+
+        if (val < root->val) {
+            return searchBST(root->left, val);
+        }
+        
+        return searchBST(root->right, val);
     }
 };
